accca5491f80866e3d46f0d8f8dd76bc711bfcb6
penguin2435coder <penguin2435coder@penguincoding.org>
2024-07-24 15:11:45 -0400

    Add files via upload

---
+++ python coding day 3.py
@@ -0,0 +1,21 @@
+'''import random
+
+num = random.randint(1,20)
+#print(num)
+
+
+
+for i in range(0, 10, 2):
+    print(i)
+
+x = 10
+while x < 20:
+    print("THIS WILL CRASH YOUR CONPUTER")
+    X += 1'''
+
+famous = input("would you like to be a famous person?")
+while famous == "yes":
+    print("OK!!!")
+    print("you are now famous. you get 100000 bucks.")
+    famous = input("are you happy with your life?")
+print("done!")
