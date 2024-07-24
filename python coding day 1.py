b98ac6a37e5a371f19637a4c15adb789b2108e7f
penguin2435coder <penguin2435coder@penguincoding.org>
2024-07-24 10:52:37 -0400

    Add files via upload
    Python days one and two, initial commit.

---
+++ python coding day 1.py
@@ -0,0 +1,48 @@
+import os
+
+'''print ("heyyyy how ya doin???")
+elephant = input ("what is your favorite type of elephant?\n")
+print ("oooo I like " + elephant + "s" + " too!")
+videogame = input ("now ive been wondering... whats your favorite video game?\n")
+print ("oh, " + videogame + " is soooo cool!!!")'''         
+
+job = input("give me a job")
+bodypart = input("give me a body part(plural)")
+num = input("give me a number")
+food = input("give me a food")
+show = input("give me a tv show")
+movie = input("give me a movie")
+electronic = input("give me an electronic")
+verbed = input("give me a verb ending in ed")
+
+print("You just started your new job as a " + job + " and when you got in your office, you found several " + bodypart + " strewen across the floor! There had to be about " + num + " of them! Now it was time for your lunch break. you packed " + food + " in your lunch today. After lunch you secretly snuck on to your " + electronic + " and started watching " + show + " and " + movie + ". The next day your boss " + verbed + " you.")  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def clear():
+    os.system("CLS")
---
+++ python coding day 2.py
@@ -0,0 +1,62 @@
+
+num1 = int(input("give me a number"))
+num2 =int(input("give me another number"))
+print ("the sum is "+ str(num1 + num2))
+print ("I am good at math!!!")                          
+input("are you good at math?")
+input("you are not as good as me.")
+answer = input("are you saying you want a challange???")
+if answer == "yes" :
+    print("ok... here we go.")
+   
+elif answer == "no" :
+    print("so you dont want to... now you will die.")
+else :
+    print:("please answer with a yes or a no. I am a conputer so I can not comprehent such language.")
+
+
+
+
+
+
+
+
+
+
+    
+'''temp = int(input("tempereture please: "))
+weather = input("Weather conditions (clear, rainy, snow, and tornado) : ")
+
+if  weather == "rainy" :
+    print ("Lock yourself in your house!!!!")
+elif weather == "tornado" :
+    print("MOVE TO THE NEXT COUNTRY OVER")
+elif temp > 90 and weather == "clear" :
+    print ("hot if your in fahreheit, VERY HOT IF YOUR IN CELSIUS")
+elif weather == "snow" :
+    print ("DRINK HOT CHOCOLATE")
+
+import random
+
+num = random.randint(1,9)
+question = input("what is your question. tell me. Tell Me. TELL MEEEEE!!!!!!!!!!!!")
+
+if num == 1 :
+    print ("yes")
+elif num == 2:
+    print("no")
+elif num == 3:
+    print ("I DONT THINK SO")
+elif num == 4:
+    print ("YEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEES!!!")
+elif num == 5:
+    print ("my sorces say NO")
+elif num == 6:
+    print ("of course!")
+elif num == 7:
+    print ("not today my goood human")
+elif num == 8:
+    print ("yup")
+elif num == 9:
+    print ("Try again at 9:43 pm")
+'''
